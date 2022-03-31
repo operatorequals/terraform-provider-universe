5b21fd0a10afa927603cc0fe7b346f88d075ecb1
John Torakis <jtorakis@skroutz.gr>
2022-03-31 13:50:10 +0300

    revert: ba2175e3 - bugs in update

--- universe/resource_universe.go
+++ universe/resource_universe.go
@@ -224,6 +224,11 @@ func callExecutor(event string, d ResourceLike, providerConfig interface{}) (boo
 		if err != nil {
 			return false, err
 		}
+		err = d.Set("config", string(payloadBytes))
+		if err != nil {
+			return false, err
+		}
+
 		log.Printf("Executed: setting data to: %s", string(payloadBytes))
 	}
 
